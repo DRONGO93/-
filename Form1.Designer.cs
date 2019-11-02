f0b690a0e54250f6643369005d09cac040b17ad4
Вкртыч Кранп <sir.drongo5309@yandex.ru>
2019-11-02 19:24:54 +0300

    Востановление до проекта 2. ТОДО: доделать интерфейс. прикрутить взаимодействие с файлами.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CheckedListRegistryKey = new System.Windows.Forms.CheckedListBox();
+            this.СomboBox = new System.Windows.Forms.ComboBox();
+            this.ButtonRunService = new System.Windows.Forms.Button();
+            this.ButtonStopService = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CheckedListRegistryKey
+            // 
+            this.CheckedListRegistryKey.FormattingEnabled = true;
+            this.CheckedListRegistryKey.Location = new System.Drawing.Point(13, 13);
+            this.CheckedListRegistryKey.Name = "CheckedListRegistryKey";
+            this.CheckedListRegistryKey.Size = new System.Drawing.Size(120, 364);
+            this.CheckedListRegistryKey.TabIndex = 0;
+            // 
+            // СomboBox
+            // 
+            this.СomboBox.FormattingEnabled = true;
+            this.СomboBox.Location = new System.Drawing.Point(218, 177);
+            this.СomboBox.Name = "СomboBox";
+            this.СomboBox.Size = new System.Drawing.Size(33, 21);
+            this.СomboBox.TabIndex = 1;
+            // 
+            // ButtonRunService
+            // 
+            this.ButtonRunService.Location = new System.Drawing.Point(176, 50);
+            this.ButtonRunService.Name = "ButtonRunService";
+            this.ButtonRunService.Size = new System.Drawing.Size(75, 23);
+            this.ButtonRunService.TabIndex = 2;
+            this.ButtonRunService.Text = "Запустить";
+            this.ButtonRunService.UseVisualStyleBackColor = true;
+            // 
+            // ButtonStopService
+            // 
+            this.ButtonStopService.Location = new System.Drawing.Point(257, 50);
+            this.ButtonStopService.Name = "ButtonStopService";
+            this.ButtonStopService.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStopService.TabIndex = 3;
+            this.ButtonStopService.Text = "Остановить";
+            this.ButtonStopService.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(418, 416);
+            this.Controls.Add(this.ButtonStopService);
+            this.Controls.Add(this.ButtonRunService);
+            this.Controls.Add(this.СomboBox);
+            this.Controls.Add(this.CheckedListRegistryKey);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox CheckedListRegistryKey;
+        private System.Windows.Forms.ComboBox СomboBox;
+        private System.Windows.Forms.Button ButtonRunService;
+        private System.Windows.Forms.Button ButtonStopService;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using System.ServiceProcess;
 
 namespace Настройщик_ПК
 {
@@ -15,6 +17,75 @@ namespace Настройщик_ПК
         public Form1()
         {
             InitializeComponent();
+        }
+        // <summary> Загрузка фармы. </summary>
+        public void LoadFormMainWindow(object sender, EventArgs e)
+        {
+            // <summary> Заполнение списка служб. </summary>
+            try
+            {
+                RegistryKey rk = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services");
+                String[] names = rk.GetSubKeyNames();
+                for (int i = 0; i < names.Length; i++)
+                {
+                    CheckedListRegistryKey.Items.Add(names[i]);
+                }
+                rk.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            // <summary> Заполнение СomboBox. </summary>
+            СomboBox.Items.Add(1); СomboBox.Items.Add(2); СomboBox.Items.Add(3); СomboBox.Items.Add(4); СomboBox.Items.Add(" ");
+        }
+
+        private void ButtonRunService_Click(object sender, EventArgs e)
+        {
+            ButtonRunService.Enabled = false;
+            ServiceRunStop(false);
+            ButtonRunService.Enabled = true;
+        }
+
+        private void ButtonStopService_Click(object sender, EventArgs e)
+        {
+            ButtonStopService.Enabled = false;
+            ServiceRunStop(true);
+            ButtonStopService.Enabled = true;
+        }
+        // <summary> Реализация запуска и остановки службы. </summary>
+        public void ServiceRunStop(bool Choice)
+        {
+            try
+            {
+                foreach (string Serv in CheckedListRegistryKey.CheckedItems)
+                {
+                    ServiceController service = new ServiceController(Serv);
+                    if (Choice == false)
+                    {
+                        if (service.Status != ServiceControllerStatus.Running)
+                        {
+                            service.Start();
+                            service.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
+                        }
+                        else { }
+                    }
+
+                    if (Choice == true)
+                    {
+                        if (service.Status == ServiceControllerStatus.Running)
+                        {
+                            service.Stop();
+                            service.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
+                        }
+                        else { }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Что-то совсем пошло не так." + ex);
+            }
         }
     }
 }
